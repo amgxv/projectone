966e4a370114f99d2462e21fe2c6c0e9e4b3d0ac
amgxv <amgxv@iesemilidarder.com>
2018-02-18 12:51:28 +0100

    1

---
+++ presentation/src/main/java/com/projectone/spring/RestaurantsController.java
@@ -0,0 +1,21 @@
+package com.projectone.spring;
+
+import com.projectone.core.base.ReadDB;
+import com.projectone.core.dao.Restaurant;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/rest/api/v1")
+public class RestaurantsController {
+
+    @RequestMapping(path = "/restaurants")//, method = RequestMethod.GET)
+    public List<Restaurant> getAll(@RequestParam(value = "name", defaultValue = "") String name) {
+        ReadDB dbManager = new ReadDB();
+        List<Restaurant> lRestaurants = dbManager.readRestaurantAPI(false);
+        return lRestaurants;
+    }
+}
